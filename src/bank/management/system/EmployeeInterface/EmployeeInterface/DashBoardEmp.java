73edb41d8a7ac54fe33424dd4bf2fc1017344068
mouadd.takhdoukhi@gmail.com <mouadd.takhdoukhi@gmail.com>
2024-03-03 00:34:55 +0100

    mouad  seconde commit

--- src/bank/management/system/EmployeeInterface/EmployeeInterface/DashBoardEmp.java
+++ src/bank/management/system/EmployeeInterface/EmployeeInterface/DashBoardEmp.java
@@ -2,6 +2,7 @@ package bank.management.system.EmployeeInterface.EmployeeInterface;
 
 import bank.management.system.Conn;
 import bank.management.system.DatabaseMethodes;
+import bank.management.system.SecondeSingUp;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,31 +11,58 @@ import java.sql.ResultSet;
 
 public class DashBoardEmp extends JFrame {
 
-    String userReference ;
-    DashBoardEmp(String userR){
+    static String userReference;
+    JLabel userTypeLabel;
+
+    DashBoardEmp(String userR) {
         super("Bank System - Dashboard");
         this.userReference = userR;
-        TypeOf();
-        getContentPane().setBackground(new Color(222,255,228));
+
+        getContentPane().setBackground(new Color(222, 255, 228));
+
         setLayout(null);
-        setSize(850,800);
-        setLocation(360,40);
+        setSize(850, 600);
+        setLocation(360, 40);
+
         setResizable(false);
+
+        String userType = TypeOf();
+
+        userTypeLabel = new JLabel("User Type: " + userType);
+        userTypeLabel.setBounds(50, 50, 200, 30);
+        add(userTypeLabel);
+
         setVisible(true);
 
     }
-    public String TypeOf (){
-        String usertype = "" ;
-        try{
+
+
+
+
+    private String TypeOf() {
+        String userType = "";
+        try {
             Conn conn = new Conn();
-            String query ="select user_id,sector from usersemp where userReference = ? ";
+            String query = "SELECT sector FROM usersemp WHERE userReference = ?";
             PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setString(1,this.userReference);
-            ResultSet resultSet =  preparedStatement.executeQuery();
-            this.userReference = resultSet.getString("sector");
-        }catch (Exception E){
-            E.printStackTrace();
+
+            preparedStatement.setString(1, userReference);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                userType = resultSet.getString("sector");
+            } else {
+                System.out.println("User not found.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error: Failed to retrieve user type.", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        return usertype;
+        return userType;
+    }
+
+    public static void main(String[] args) {
+        new DashBoardEmp(userReference);
     }
 }
